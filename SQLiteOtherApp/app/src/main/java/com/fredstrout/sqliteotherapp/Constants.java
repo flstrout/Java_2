c85b91480ac4a5768ae361ad2e3d0cee4f1f716e
flstrout <FredStrout617@fullsail.edu>
2015-10-22 00:05:55 -0500

    Second app gets data from the first app!

---
+++ SQLiteOtherApp/app/src/main/java/com/fredstrout/sqliteotherapp/Constants.java
@@ -0,0 +1,24 @@
+package com.fredstrout.sqliteotherapp;
+
+import android.net.Uri;
+
+public class Constants {
+
+    private static final String AUTHORITY = "com.fredstrout.sqliteapp.opportunityprovider";
+    private static final String BASE_PATH = "opportunities";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+    //Constants for db name and version
+    private static final String DATABASE_NAME = "opportunities.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //Constants for identifying table and columns
+    public static final String TABLE_OPPORTUNITIES = "opportunities";
+    public static final String OPPORTUNITY_ID = "_id";
+    public static final String OPPORTUNITY_NAME = "opportunityName";
+    public static final String OPPORTUNITY_RESOLUTION = "opportunityResolution";
+    public static final String OPPORTUNITY_PROBLEM = "opportunityProblem";
+    public static final String OPPORTUNITY_CREATED = "opportunityCreated";
+
+    public static final String [] ALL_COLUMNS = {OPPORTUNITY_ID, OPPORTUNITY_NAME, OPPORTUNITY_RESOLUTION, OPPORTUNITY_PROBLEM, OPPORTUNITY_CREATED};
+}
---
+++ SQLiteOtherApp/app/src/main/java/com/fredstrout/sqliteotherapp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.fredstrout.sqliteotherapp;
+
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private CursorAdapter adapter;
+    public static Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this;
+
+        String[] from = {Constants.OPPORTUNITY_NAME};
+        int[] to = {android.R.id.text1};
+        adapter = new SimpleCursorAdapter(mContext, android.R.layout.simple_list_item_1, null, from, to, 0);
+
+        ListView lv = (ListView) findViewById(android.R.id.list);
+        lv.setAdapter(adapter);
+
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                Toast.makeText(mContext, "Settings Clicked!", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(mContext, Constants.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
+    }
+}
